3bf24aafb5965ccb954a740110d93e34fc5e1fa5
Vergueirod <diego.ssv@outlook.com>
2024-02-01 09:43:08 -0300

    Function get_pedidos_items() creation

--- apis_tests/retorno_pedidos.py
+++ apis_tests/retorno_pedidos.py
@@ -1,17 +1,21 @@
 import json
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
 
 def carregar_dados_json():
     with open('../teste_pedidos_response.json', 'r') as arquivo:
         dados_json = json.load(arquivo)
     return dados_json
 
+
 @app.get("/all-pedido")
 def parse_pedido():
     dados_pedido = carregar_dados_json()
     return dados_pedido
+
 
 @app.get("/pedidos-last")
 def get_pedidos_last():
@@ -28,6 +32,7 @@ def get_pedidos_last():
     else:
         return {"mensagem": "Nenhum pedido encontrado."}
     
+    
 @app.get("/pedidos-list")
 def get_pedidos_list():
     dados_pedido = parse_pedido()
@@ -42,3 +47,26 @@ def get_pedidos_list():
         })
 
     return lista_pedidos
+
+
+@app.get("/pedidos-items")
+def get_pedidos_items():
+    dados_pedido = parse_pedido()
+    pedidos = dados_pedido['retorno']['pedidos']
+    item_list = []
+    
+    for i in pedidos:
+        itens = i['pedido']['itens']
+        for j in itens:
+            item = j['item']
+
+            item_list.append({
+                "Código": item['codigo'],
+                "Descrição": item['descricao'],
+                "Quantidade": item['quantidade'],
+                "Valor": item['valorunidade'],
+                "Peso": item['pesoBruto'],
+            })
+    
+    return item_list
+    
